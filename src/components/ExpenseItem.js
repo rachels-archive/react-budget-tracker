afdf2be6653baef5dbdbe6be2a975db95f57588a
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2022-03-25 22:04:21 +0800

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -16,7 +16,7 @@ function ExpenseItem(props) {
 		<li className='list-group-item d-flex justify-content-between align-items-center'>
 			{props.name}
 			<div>
-				<span className='badge rounded-pill bg-primary mr-3'>£{props.cost}</span>
+				<span className='badge rounded-pill bg-primary mr-3'>${props.cost}</span>
 				<TiDelete size='1.5em' onClick={handleDeleteExpense} />
 			</div>
 		</li>
